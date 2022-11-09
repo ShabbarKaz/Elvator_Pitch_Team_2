369ad9c4bbc6aabaca317e83d1e6ed009b6144a0
ShabbarKazmi <shabbarhassan329@gmail.com>
2022-11-09 14:25:09 -0600

    Added a place holdermap and I also added a NavBarMenu.

--- UWOsh_InteractiveMap/NavBar.xaml.cs
+++ UWOsh_InteractiveMap/NavBar.xaml.cs
@@ -6,4 +6,42 @@ public partial class NavBar : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    void OnTextChanged(object sender, EventArgs e)
+    {
+        SearchBar.Text = "TextChanged";
+    }
+
+    async void GoToListPlants(object sender, EventArgs args)
+    {
+        await Shell.Current.GoToAsync(nameof(ListAllPlants));
+    }
+
+    async void GetCompass(object sender, EventArgs args)
+    { 
+        await Shell.Current.GoToAsync(nameof(SearchHistory));
+    }
+
+    async void GiveFeedback(object sender, EventArgs args)
+    {
+        if (Email.Default.IsComposeSupported)
+        {
+
+            string subject = "Issue found";
+            string body = "";
+            string[] recipients = new[] { "Kazmis55@uwosh", "Kazmis55@Uwosh.edu" };
+
+            var message = new EmailMessage
+            {
+                Subject = subject,
+                Body = body,
+                BodyFormat = EmailBodyFormat.PlainText,
+                To = new List<string>(recipients)
+            };
+
+            FeedBack.Text = "Feed Back sent";
+
+            // await Email.Default.ComposeAsync(message);
+        }
+    }
 }
