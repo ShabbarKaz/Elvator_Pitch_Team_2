894f0fe1e8a04ed825b48323f4ed33b6825db7f7
Rudlin0 <rudyliljeberg@gmail.com>
2022-11-09 18:09:45 -0600

    Added Search History page

---
+++ SearchHistory.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace UWOsh_InteractiveMap;
+
+public partial class SearchHistory : ContentPage
+{
+	public SearchHistory()
+	{
+		InitializeComponent();
+	}
+
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        
+    }
+}
