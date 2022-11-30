36e97ba46eef848522d403394a32e66a093656de
Benjamin <vainin@users.noreply.github.com>
2022-11-30 17:50:38 -0600

    angle B added

--- UWOsh_InteractiveMap/Compass.xaml.cs
+++ UWOsh_InteractiveMap/Compass.xaml.cs
@@ -18,9 +18,10 @@ public partial class Compass : ContentPage
 	 a = Location.CalculateDistance(UserLocation , PlantLocation x  UserLocation y)
 
 	A = arccos((b*b + c*c - a*a) / (2bc)) opposite angle of side a
-	Location boston = new Location(42.358056, -71.063611);
-	Location sanFrancisco = new Location(37.783333, -122.416667);
+	B = arccos((a*a + c*c - b*b) / (2ac)) opposite angle of side b
+	Location self = new Location(42.358056, -71.063611);
+	Location plant = new Location(37.783333, -122.416667);
 
-	double miles = Location.CalculateDistance(boston, sanFrancisco, DistanceUnits.Miles);
+	double clength = Location.CalculateDistance(self, plant, DistanceUnits.Kilometers);
 	 */
 }
