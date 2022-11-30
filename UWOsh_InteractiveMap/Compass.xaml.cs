82c2e14189fa177f10520b9a25b70d5c148e16a7
Benjamin <vainin@users.noreply.github.com>
2022-11-30 15:52:56 -0600

    notes for location finding

--- UWOsh_InteractiveMap/Compass.xaml.cs
+++ UWOsh_InteractiveMap/Compass.xaml.cs
@@ -6,4 +6,21 @@ public partial class Compass : ContentPage
 	{
 		InitializeComponent();
 	}
+	
+	/*Distance function
+	 * 
+	      c  /|  b
+	        / |
+	        ---
+	        a
+	 c = Location.CalculateDistance(UserLocation , PlantLocation)
+	 b = Location.CalculateDistance(PlantLocation x UserLocation y   , PlantLocation)
+	 a = Location.CalculateDistance(UserLocation , PlantLocation x  UserLocation y)
+
+	A = arccos((b*b + c*c - a*a) / (2bc)) opposite angle of side a
+	Location boston = new Location(42.358056, -71.063611);
+	Location sanFrancisco = new Location(37.783333, -122.416667);
+
+	double miles = Location.CalculateDistance(boston, sanFrancisco, DistanceUnits.Miles);
+	 */
 }
